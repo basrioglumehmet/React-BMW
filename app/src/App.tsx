083f75a4a1569fe1edbaad729d1f4711e4e37618
Son Kod Bükücü <basrioglumehmet@gmail.com>
2024-08-03 15:52:28 +0300

    Keep calm and commit

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,17 @@
-function App() {
-  return (
-    <>
-      <div>
-        React BMW Application
-     </div>
-    </>
-  )
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+class App extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
 
 export default App
